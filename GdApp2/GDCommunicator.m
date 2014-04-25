bc9fab8a62e9a70a7c3583d88dbcf57e95a081b3
Guo, Xing Hua <gekowa@gmail.com>
2014-04-26 01:12:22 +0800

    Should use main queue when requesting JSON

--- GdApp2/GDCommunicator.m
+++ GdApp2/GDCommunicator.m
@@ -12,17 +12,20 @@
 @implementation GDCommunicator
 
 - (void)fetchHomeInfo {
+    NSLog(@"fetchHomeInfo starts");
     NSString *urlAsString = [NSString stringWithFormat:@"http://www.sdgundam.cn/app/test.json?r=%d", rand()];
     NSURL *url = [[NSURL alloc] initWithString:urlAsString];
     
     [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:url]
-                                       queue:[[NSOperationQueue alloc] init]
+                                       queue:[NSOperationQueue mainQueue]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               NSLog(@"fetchHomeInfo completed");
                                if (connectionError) {
                                    [self.delegate fetchHomeInfoFailedWithError:connectionError];
                                } else {
                                    [self.delegate receivedHomeInfoJSON:data];
                                }
                            }];
+    NSLog(@"fetchHomeInfo ends");
 }
 @end
