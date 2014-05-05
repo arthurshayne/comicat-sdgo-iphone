af9334ae54015184332be9420ae18cb55c94049a
Guo, Xing Hua <gekowa@gmail.com>
2014-05-05 09:58:27 +0800

    Finished the tab bar images and launching images

---
+++ GdApp2/GDTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  GDTabBarController.h
+//  GdApp2
+//
+//  Created by Guo, Xing Hua on 5/5/14.
+//  Copyright (c) 2014 COMICAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GDTabBarController : UITabBarController
+
+@end
---
+++ GdApp2/GDTabBarController.m
@@ -0,0 +1,51 @@
+//
+//  GDTabBarController.m
+//  GdApp2
+//
+//  Created by Guo, Xing Hua on 5/5/14.
+//  Copyright (c) 2014 COMICAT. All rights reserved.
+//
+
+#import "GDTabBarController.h"
+
+@interface GDTabBarController ()
+
+@end
+
+@implementation GDTabBarController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+    UITabBarItem *tabBarItem0 = [self.tabBar.items objectAtIndex:0];
+    tabBarItem0.image = [[UIImage imageNamed:@"tab-home"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    tabBarItem0.selectedImage = [[UIImage imageNamed:@"tab-home-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    UITabBarItem *tabBarItem1 = [self.tabBar.items objectAtIndex:1];
+    tabBarItem1.image = [[UIImage imageNamed:@"tab-video"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    tabBarItem1.selectedImage = [[UIImage imageNamed:@"tab-video-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    UITabBarItem *tabBarItem2 = [self.tabBar.items objectAtIndex:2];
+    tabBarItem2.image = [[UIImage imageNamed:@"tab-news"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    tabBarItem2.selectedImage = [[UIImage imageNamed:@"tab-news-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    UITabBarItem *tabBarItem3 = [self.tabBar.items objectAtIndex:3];
+    tabBarItem3.image = [[UIImage imageNamed:@"tab-unit"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    tabBarItem3.selectedImage = [[UIImage imageNamed:@"tab-unit-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
